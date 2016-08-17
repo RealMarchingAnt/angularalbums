91c765545eb591e9718406cad671eddd5040bf50
Patrik Johansson <patrik.marchingant.johansson@gmail.com>
2016-08-17 20:42:00 +0200

    Add /albums/:id route and tests

--- routes/albums.js
+++ routes/albums.js
@@ -1,12 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-/* GET albums listing. */
+var mockalbums = [
+  {
+    _id: "123",
+    title: "Album 1",
+    description: "This is my albums",
+    coverimage: "test.png"
+  },
+    {
+    _id: "456",
+    title: "Album 2",
+    description: "This is my second album",
+    coverimage: "test2.png"
+  }
+
+];
+
+router.use(bodyParser.json());
+
+/**
+ * Returns all albums
+ */
 router.get('/', function(req, res, next) {
-  res.json({
-    name: "Invention of Knowledge",
-    artist: "Anderson/Stolt"
-  });
+  res.json(mockalbums[0]);
+});
+
+/**
+ * Return a specific album
+ */
+router.get('/:id', function(req, res, next) {
+  
+  var id = req.params.id;
+
+  var result = mockalbums.filter(function(obj) {
+    return obj._id == id;
+  })[0];
+
+  if (!result) {
+    console.log("Album with id " + id + " not found");
+    res.sendStatus(404);
+  }
+
+  res.json(result);
+
 });
 
 module.exports = router;
--- test/test.js
+++ test/test.js
@@ -2,17 +2,6 @@ var assert = require('assert');
 var request = require('supertest');
 var app = require('../app');
 
-// describe('Array', function() {
-//     describe('#indexOf()', function() {
-//         it('should return -1 when the value is not present', function() {
-//             assert.equal(-1, [1,1,3].indexOf(4));
-//         });
-//     });
-// });
-
-
-
-
 describe('GET /albums', function () {
 
     it('Response status OK', function (done) {
@@ -36,5 +25,44 @@ describe('GET /albums', function () {
             });
     });
 
+
+});
+
+describe('GET /albums/123', function () {
+
+    it('Returns status OK', function (done) {
+        request(app)
+            .get('/albums/123')
+            .expect(200)
+            .end(function (error) {
+                if (error) throw error;
+                done();
+            });
+    });
+
+    it('Returns JSON data', function (done) {
+        request(app)
+            .get('/albums/123')
+            .expect('Content-Type', /json/)
+            .end(function (error) {
+                if (error) throw error;
+                done();
+            });
+    });
+
+    it('Returns an album with title, description, and cover image url', function (done) {
+        request(app)
+            .get('/albums/456')
+            .expect(200, {
+                _id: '456',
+                title: 'Album 2',
+                description: "This is my second album",
+                coverimage: "test2.png"
+            })
+            .end(function (error) {
+                if (error) throw error;
+                done();
+            });
+    });
 
 });
